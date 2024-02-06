d46704a78a3814932788cc496e0fba891413d0ea
Robin Monseré <robin.monsere@student.howest.be>
2024-02-06 18:13:41 +0100

    adding speedinsights

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,5 @@
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 
+
 inject({ mode: dev ? 'development' : 'production' });
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
     import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    injectSpeedInsights();
+    inject({ mode: dev ? 'development' : 'production' });
+
 </script>
 
 <slot/>
